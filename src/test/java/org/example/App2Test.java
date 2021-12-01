2d91b489e6871f9c3cd9d976f77b5e987850cf6d
Nata1985 <natafufka23@rambler.ru>
2021-12-01 17:54:37 +0300

    second commit

---
+++ src/test/java/org/example/App2Test.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class App2Test {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+    @Test
+    public void shouldAnswerWithTrue1(){
+        System.out.print ("Test");
+    }
+}
